76ec91a5d01fd4b1cfd861b4a83263aad6dec51c
danielmacielbenicio <danielmacielbenicio@gmail.com>
2025-11-03 01:55:09 -0300

    config controller

---
+++ src/main/java/br/com/danielmaciel/todolist/controller/TodoController.java
@@ -0,0 +1,38 @@
+package br.com.danielmaciel.todolist.controller;
+
+import br.com.danielmaciel.todolist.entity.Todo;
+import br.com.danielmaciel.todolist.repository.TodoRepository;
+import br.com.danielmaciel.todolist.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todo")
+
+public class TodoController {
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo) {
+    return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list() {
+    return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo) {
+    return todoService.update(todo);
+    }
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable id) {
+    return todoService.delete(id);
+    }
+}
