746608651846e5c1d811cfb8ff3ea58fc5fb1750
danielmacielbenicio <danielmacielbenicio@gmail.com>
2025-11-03 01:00:01 -0300

    add files

---
+++ src/test/java/br/com/danielmaciel/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielmaciel.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
